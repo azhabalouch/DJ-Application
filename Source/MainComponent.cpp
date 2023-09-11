a1db3e9e491cdfd4b5c9ba20d05aafb10846d32f
Azha Balouch <azha.nawaz@belouch.com>
2023-09-11 22:54:39 +0500

    Added Audio File Playback Functionality
    **Introduction**:
    In this commit, I've added the functionality to play audio files using the JUCE framework. This task involved setting up an `AudioFormatManager` to handle different audio file formats and creating an `AudioTransportSource` to manage audio playback.
    
    **Setting up the AudioFormatManager**:
    - Introduced an `AudioFormatManager` in the `MainComponent.h` file, allowing us to work with various audio file formats like mp3 and WAV.
    - Registered basic audio formats using `formatManager.registerBasicFormats()` to ensure compatibility.
    
    **Setting up an AudioTransportSource**:
    - Added an `AudioTransportSource` variable in the `MainComponent.h` file to manage audio data from audio files.
    - Configured `transportSource` in the `prepareToPlay` and `releaseResources` functions to prepare for audio playback.
    - Modified the `getNextAudioBlock` function to delegate audio buffering to the `transportSource`.
    
    **Providing a File to the AudioTransportSource**:
    - Introduced a `std::unique_ptr<AudioFormatReaderSource>` variable named `readerSource` in `MainComponent.h` for handling audio file reading.
    - Created a hard-coded URL to an audio file, which can be customized for different systems.
    - Implemented the process of reading and loading audio files using the `formatManager.createReaderFor` method and wrapped it in a unique pointer to ensure proper resource management.
    
    **Making the Start, Stop, and Gain Controls Work**:
    - Enabled the play button to rewind and play the audio file from the start when clicked.
    - Implemented the stop button to halt audio playback.
    - Linked the gain slider to adjust the audio volume.
    
    **Making a File Chooser**:
    - Added a "LOAD" button to the interface for selecting audio files.
    - Integrated a file chooser component (`juce::FileChooser`) to allow users to browse and select audio files.
    - Implemented the event listener for the "LOAD" button to launch the file chooser asynchronously and load selected files.
    
    **Clearing Up with a loadURL Function**:
    - Created a `loadURL` function to centralize the code for loading audio files, promoting code reusability.
    - Refactored the button click event for the "LOAD" button to use the `loadURL` function, reducing redundancy.
    
    With these changes, the application can now play audio files, adjust volume, and load files dynamically through the file chooser. This commit enhances the audio playback capabilities of the project, making it more versatile and user-friendly.

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,25 +1,20 @@
 #include "MainComponent.h"
 
-//==============================================================================
 MainComponent::MainComponent()
 {
-    // Make sure you set the size of the component after
-    // you add any child components.
-    setSize (800, 600);
-
-    // Some platforms require permissions to open input channels so request that here
-    if (juce::RuntimePermissions::isRequired (juce::RuntimePermissions::recordAudio)
-        && ! juce::RuntimePermissions::isGranted (juce::RuntimePermissions::recordAudio))
+    setSize(800, 600);
+    if (juce::RuntimePermissions::isRequired(juce::RuntimePermissions::recordAudio)
+        && !juce::RuntimePermissions::isGranted(juce::RuntimePermissions::recordAudio))
     {
-        juce::RuntimePermissions::request (juce::RuntimePermissions::recordAudio,
-                                           [&] (bool granted) { setAudioChannels (granted ? 2 : 0, 2); });
+        juce::RuntimePermissions::request(juce::RuntimePermissions::recordAudio,
+            [&](bool granted) { setAudioChannels(granted ? 2 : 0, 2); });
     }
     else
     {
-        // Specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
+        setAudioChannels(2, 2);
     }
 
+    formatManager.registerBasicFormats();
 
     addAndMakeVisible(playButton);
     playButton.addListener(this);
@@ -30,77 +25,60 @@ MainComponent::MainComponent()
     addAndMakeVisible(gainSlider);
     gainSlider.addListener(this);
 
+    addAndMakeVisible(loadButton);
+    loadButton.addListener(this);
+    loadButton.setButtonText("LOAD");
+
     gainSlider.setRange(0, 1);
 
+    DBG("MainComponent::resized");
 }
 
 MainComponent::~MainComponent()
 {
-    // This shuts down the audio device and clears the audio source.
     shutdownAudio();
 }
 
-//==============================================================================
-void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
     playing = false;
     gain = 0.5;
     phase = 0;
     dphase = 0;
+
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
 }
 
-void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
+void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
 {
     if (!playing)
     {
         bufferToFill.clearActiveBufferRegion();
         return;
     }
-
-    auto* leftChannel = bufferToFill.buffer->getWritePointer(0, bufferToFill.startSample);
-    auto* rightChannel = bufferToFill.buffer->getWritePointer(1, bufferToFill.startSample);
-    
-    for (auto i = 0; i < bufferToFill.numSamples; ++i)
-    {
-        auto sample = fmod(phase, 1.0f);
-        phase += fmod(dphase, 0.01f);
-        dphase += 0.0000005f;
-        leftChannel[i] = sample * 0.125 * gain;
-        rightChannel[i] = sample * 0.125 * gain;
-    }
-
-    //bufferToFill.clearActiveBufferRegion();
+    transportSource.getNextAudioBlock(bufferToFill);
 }
 
 void MainComponent::releaseResources()
 {
-    // This will be called when the audio device stops, or when it is being
-    // restarted due to a setting change.
-
-    // For more details, see the help for AudioProcessor::releaseResources()
+    transportSource.releaseResources();
 }
 
-//==============================================================================
-void MainComponent::paint (juce::Graphics& g)
+void MainComponent::paint(juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    // You can add your drawing code here!
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainContentComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
-
     int rowH = getHeight() / 5;
     int colW = getWidth();
 
     playButton.setBounds(0, 0, colW, rowH);
     stopButton.setBounds(0, rowH, colW, rowH);
-    gainSlider.setBounds(0, rowH * 2, colW, rowH);
+    loadButton.setBounds(0, rowH * 2, colW, rowH);
+    gainSlider.setBounds(0, rowH * 3, colW, rowH);
+
 
     DBG("MainComponent::resized");
 }
@@ -111,10 +89,25 @@ void MainComponent::buttonClicked(juce::Button* button)
     {
         playing = true;
         dphase = 0;
+        transportSource.setPosition(0);
+        transportSource.start();
     }
+
     if (button == &stopButton)
     {
         playing = false;
+        transportSource.stop();
+    }
+
+    if (button == &loadButton)
+    {
+        auto fileChooserFlags = juce::FileBrowserComponent::canSelectFiles;
+        fChooser.launchAsync(fileChooserFlags,
+            [this](const juce::FileChooser& chooser)
+            {
+                auto chosenFile = chooser.getResult();
+                loadURL(juce::URL{ chosenFile });
+            });
     }
 }
 
@@ -123,6 +116,22 @@ void MainComponent::sliderValueChanged(juce::Slider* slider)
     if (slider == &gainSlider)
     {
         gain = gainSlider.getValue();
+        transportSource.setGain(gain);
     }
 
+}
+
+void MainComponent::loadURL(juce::URL audioURL)
+{
+    auto* reader = formatManager.createReaderFor(audioURL.createInputStream(false));
+    if (reader != nullptr)
+    {
+        std::unique_ptr<juce::AudioFormatReaderSource> newSource(new juce::AudioFormatReaderSource(reader, true));
+        transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
+        readerSource.reset(newSource.release());
+    }
+    else
+    {
+        std::cout << "Something went wrong loading the file " << std::endl;
+    }
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,47 +2,41 @@
 
 #include <JuceHeader.h>
 
-//==============================================================================
-/*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
-*/
-class MainComponent  : public juce::AudioAppComponent,
-                       public juce::Button::Listener,
-                       public juce::Slider::Listener
+class MainComponent : public juce::AudioAppComponent,
+    public juce::Button::Listener,
+    public juce::Slider::Listener
 {
 public:
-    //==============================================================================
     MainComponent();
     ~MainComponent() override;
 
-    //==============================================================================
-    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
-    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
 
-    //==============================================================================
-    void paint (juce::Graphics& g) override;
+    void paint(juce::Graphics& g) override;
     void resized() override;
 
     void buttonClicked(juce::Button* button) override;
     void sliderValueChanged(juce::Slider* slider) override;
 
+    void loadURL(juce::URL audioURL);
 
 private:
-    //==============================================================================
-    // Your private member variables go here...
-
     juce::TextButton playButton{ "Play" };
     juce::TextButton stopButton{ "Stop" };
-
+    juce::TextButton loadButton;
     juce::Slider gainSlider;
+    juce::AudioTransportSource transportSource;
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::FileChooser fChooser{ "Select a file..." };
 
-    juce::Random random;
+
     bool playing;
     double gain;
     float phase;
     double dphase;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
